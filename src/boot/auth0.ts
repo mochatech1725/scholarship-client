5a67594cd7a351f28de62f30935f2e7772f6ca7b
TeialD <teial.starks@gmail.com>
2025-06-07 10:31:04 -0700

    more

--- src/boot/auth0.ts
+++ src/boot/auth0.ts
@@ -4,12 +4,13 @@ import { createAuth0 } from '@auth0/auth0-vue'
 // Debug environment variables
 console.log('Auth0 Domain:', import.meta.env.VITE_AUTH0_DOMAIN)
 console.log('Auth0 Client ID:', import.meta.env.VITE_AUTH0_CLIENT_ID)
+console.log('Auth0 Callback URL:', import.meta.env.VITE_AUTH0_CALLBACK_URL)
 
 const auth0Config = {
   domain: import.meta.env.VITE_AUTH0_DOMAIN,
   clientId: import.meta.env.VITE_AUTH0_CLIENT_ID,
   authorizationParams: {
-    redirect_uri: `${window.location.origin}/callback`,
+    redirect_uri: import.meta.env.VITE_AUTH0_CALLBACK_URL,
     screen_hint: "signup",
   }
 }
